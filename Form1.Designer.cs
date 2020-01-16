53b3d7675bae571191eed3198866a13e83468c1d
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2020-01-15 21:27:43 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DVD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dvd = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dvd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dvd
+            // 
+            this.dvd.Image = global::DVD.Properties.Resources.logo;
+            this.dvd.Location = new System.Drawing.Point(235, 72);
+            this.dvd.Name = "dvd";
+            this.dvd.Size = new System.Drawing.Size(131, 83);
+            this.dvd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dvd.TabIndex = 0;
+            this.dvd.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dvd);
+            this.Name = "Form1";
+            this.Text = "What Am I Doing With My Life?";
+            ((System.ComponentModel.ISupportInitialize)(this.dvd)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dvd;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVD
+{
+    public partial class Form1 : Form
+    {
+        private double speedX = 2.5, speedY = 2.5;
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1;
+            timer1.Start();
+
+            dvd.Left = rand.Next(0, this.Width - dvd.Width);
+            dvd.Top = rand.Next(0, this.Height - dvd.Height - 5);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dvd.Top -= (int) speedY;
+            dvd.Left -= (int) speedX;
+
+            if (dvd.Top < 0 || dvd.Bottom >= this.Height - 30)
+            {
+                speedY *= -1;
+            }
+            if (dvd.Left < 0 || dvd.Right >= this.Width - 10)
+            {
+                speedX *= -1;
+            }
+        }
+    }
+}
